ecd748810cd5a189cb901bacd6143f41a2610217
Josh Burt <jburt@anaconda.com>
2023-03-11 09:43:23 -0800

    version bump

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@ sys.path.insert(0, os.path.abspath("../../backend"))
 project = "MLFlow Backend Plugin For Anaconda Enterprise Projects"
 copyright = "2023, Anaconda, Inc"
 author = "Joshua C. Burt"
-release = "0.4.0"
+release = "0.5.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="anaconda.enterprise.mlflow.plugin.backend.project",
-    version="0.4.0",
+    version="0.5.0",
     description="MLFlow Backend Plugin For Anaconda Enterprise Projects",
     package_dir={"": "src"},
     packages=setuptools.find_namespace_packages(where="src"),
