316ef80a870301ba563a03497120f4d68f81543a
Josh Burt <jburt@anaconda.com>
2023-03-12 17:32:04 -0700

    versioneer updates

--- mlflow_adsp/__init__.py
+++ mlflow_adsp/__init__.py
@@ -1,9 +1,12 @@
 """ mlflow-asdp namespace """
 
+from . import _version
 from .backend import AnacondaEnterpriseProjectBackend, ae_backend_builder
 from .contacts.errors.plugin import AEMLFlowPluginError
 from .services.worker import WorkerService
 from .submitted_run import AnacondaEnterpriseSubmittedRun
+
+__version__ = _version.get_versions()["version"]
 
 from . import _version
 __version__ = _version.get_versions()['version']
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
-from setuptools import setup
 import versioneer
+from setuptools import setup
 
 setup(
     name="mlflow-adsp",
