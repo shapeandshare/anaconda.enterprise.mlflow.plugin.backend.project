3fe382f0c4a8e9f72ce80fd855e9e3751468bcb2
Josh Burt <jburt@anaconda.com>
2023-03-13 08:21:07 -0700

    linting

--- mlflow_adsp/__init__.py
+++ mlflow_adsp/__init__.py
@@ -1,9 +1,9 @@
 """ mlflow-asdp namespace """
 
+from . import _version
 from .backend import AnacondaEnterpriseProjectBackend, ae_backend_builder
 from .contacts.errors.plugin import AEMLFlowPluginError
 from .services.worker import WorkerService
 from .submitted_run import AnacondaEnterpriseSubmittedRun
 
-from . import _version
-__version__ = _version.get_versions()['version']
+__version__ = _version.get_versions()["version"]
